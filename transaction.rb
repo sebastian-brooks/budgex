268aae4ae2538b1b86addeda3b7953f17f9c6f6b
Seb Brooks <sbrookstree@gmail.com>
2020-09-25 17:24:12 +1000

    added get_trans_date function to capture transaction date

--- transaction.rb
+++ transaction.rb
@@ -1,7 +1,8 @@
 require "csv"
+require "date"
 
 class Transaction
-    def initialize(user, id, date, amount, description="", category="general", recur=0)
+    def initialize(user, id, date, amount, description, category, recur=0)
         @user = user
         @id = id
         @date = date
@@ -23,3 +24,25 @@ def get_id(user)
     file = CSV.parse(File.read("#{user}.csv"))
     new_id = file.size + 1
 end
+
+def get_trans_date
+    date = nil
+    while date.nil?
+        puts "Please enter the transaction date [FORMAT: YYYY-MM-DD (e.g. Dec 31st 1995 = 1995-12-31)]"
+        date = gets.chomp
+        begin
+            Date.iso8601(date)
+        rescue ArgumentError
+            date = nil
+            puts "That date is not valid."
+        end
+    end
+    if date.length < 10
+        date = Date.strptime(date, "%y-%m-%d")
+    end
+    return date
+end
+t_date = get_trans_date
+# t_amt = get_trans_amt
+# t_desc = get_trans_desc
+# t_cat = get_trans_cat
