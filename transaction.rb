4aeea284179d18e78d2e266b0b1693d644a1a7a2
Seb Brooks <sbrookstree@gmail.com>
2020-09-25 16:31:40 +1000

    Transaction.add method and get_id function added

--- transaction.rb
+++ transaction.rb
@@ -1,9 +1,25 @@
+require "csv"
+
 class Transaction
-    def initialize(date, amount, description, category, recur)
+    def initialize(user, id, date, amount, description="", category="general", recur=0)
+        @user = user
+        @id = id
         @date = date
         @amount = amount
         @description = description
         @category = category
         @recur = recur
+        @trans = {id: @id, date: @date, amount: @amount, description: @description, category: @category, recur: @recur}
     end
+
+    def add
+        CSV.open("#{@user}.csv", "a") do |row|
+            row << @trans.values.to_a
+        end
+    end
+end
+
+def get_id(user)
+    file = CSV.parse(File.read("#{user}.csv"))
+    new_id = file.size + 1
 end
