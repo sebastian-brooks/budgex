6eec106e99e8d7c832cb59df7ceb6673c9333574
Seb Brooks <sbrookstree@gmail.com>
2020-09-26 12:12:24 +1000

    added get balance and check sub zero functions

--- budgex.rb
+++ budgex.rb
@@ -1,5 +1,6 @@
 require_relative("functions/add_transactions")
 require_relative("functions/search_transactions")
+require_relative("functions/get_balance")
 
 # Run login/signup
 # main menu
@@ -27,6 +28,9 @@ puts "3 - Search transaction by date"
 puts "4 - Search transactions by date range"
 puts "5 - Search transactions by category"
 puts "6 - Search transactions by date range and category"
+puts "7 - Get current balance"
+puts "8 - Get future balance"
+puts "9 - Check for sub-zero balance danger"
 
 opt = gets.chomp.to_i
 
@@ -43,6 +47,12 @@ when 5
     search_trans_by_cat(username)
 when 6
     search_trans_by_cat_date_range(username)
+when 7
+    get_balance(username)
+when 8
+    get_balance(username, 1)
+when 9
+    sub_zero_balance_check(username)
 else
     puts "You fucked up. I'm outta here!"
 end
--- functions/get_balance.rb
+++ functions/get_balance.rb
@@ -1,2 +1,56 @@
 require "csv"
+require_relative("get_date")
 
+def retrieve_stored_balance(user)
+    bal = {}
+    CSV.foreach("user_transactions/#{user}_balance.csv", headers: true).select { |row|
+        bal[:date] = row["date"]
+        bal[:balance] = row["balance"].to_f
+    }
+    return bal
+end
+
+def get_balance(user, fut=0)
+    date = ""
+    if fut == 1
+        puts "Please enter the future date you'd like to get your balance for [FORMAT: YYYY-MM-DD (e.g. Dec 31st 1995 = 1995-12-31)]"
+        puts "Leave blank for today's date"
+        date = get_date
+    else
+        date = Date.today.to_s
+    end
+    bal_date = retrieve_stored_balance(user)[:date]
+    bal_amt = retrieve_stored_balance(user)[:balance]
+    CSV.foreach("user_transactions/#{user}.csv", headers: true).select { |row|
+        if row["date"] >= bal_date && row["date"] <= date
+            bal_amt += row["amount"].to_f
+        end
+    }
+    if fut == 1
+        puts "Your predicted balance is: #{bal_amt.to_d}"
+    else
+        puts "Your current balance is: #{bal_amt.to_d}"
+    end
+    return bal_amt
+end
+
+def sub_zero_balance_check(user)
+    curr_bal = get_balance(user)
+    date = Date.today.to_s
+    sub_z = nil
+    scary_dates = []
+    CSV.foreach("user_transactions/#{user}.csv", headers: true).select { |row|
+        if row["date"] > date
+            curr_bal += row["amount"].to_f
+            if curr_bal < 0
+                sub_z = row["date"]
+                scary_dates << row["date"]
+            end
+        end
+    }
+    if scary_dates.size == 0
+        puts "Great news! You should have enough funds for all your scheduled expenses! Keep up the great work!"
+    else
+        puts "Oh no! You won't have enough money on #{scary_dates.sort[0]}!"
+    end
+end
